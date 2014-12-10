a7ebccb6b43957c7745143fe8374d3b9b73aa09a
Taivo Käsper <taivo.kasper@gmail.com>
2014-12-11 00:24:12 +0200

    Impovements if initial processing takes long time

--- gource_generator.bash
+++ gource_generator.bash
@@ -7,13 +7,14 @@ screen -dmS recording xvfb-run -a -s "-screen 0 1280x720x24" gource -1280x720 -r
 
 sleep 3
 filesize=$(stat -c '%s' /results/gource.ppm)
-while [ $filesize -lt $(stat -c '%s' /results/gource.ppm) ];
+while [[ "$filesize" -eq "0" || $filesize -lt $(stat -c '%s' /results/gource.ppm) ]] ;
 do
-	sleep 2
-    echo 'Polling the size'
+	sleep 10
+    echo 'Polling the size. Current size is' $filesize
     filesize=$(stat -c '%s' /results/gource.ppm)
 done
 echo 'Force stopping recording because file size is not growing'
 screen -S recording -X quit
 
 xvfb-run -a -s "-screen 0 1280x720x24" ffmpeg -y -r 30 -f image2pipe -loglevel info -vcodec ppm -i /results/gource.ppm -vcodec libx264 -preset medium -pix_fmt yuv420p -crf 1 -threads 0 -bf 0 /results/gource.mp4
+rm -f /results/gource.ppm
