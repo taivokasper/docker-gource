09f2bd60336f557161d3beef123cd6374c833fb2
bibby <andrew.bibby@nantomics.com>
2015-06-23 10:26:02 -0700

    better growth watching; configurable resolution

--- gource_generator.bash
+++ gource_generator.bash
@@ -2,20 +2,24 @@
 set -e # stop on first error
 
 rm -f /results/{gource.ppm,gource.mp4}
+RES="${RES:-1280x720}"
+DEPTH="${DEPTH:-24}"
 
-screen -dmS recording xvfb-run -a -s "-screen 0 1280x720x24" gource -1280x720 -r 30 --title "$TITLE" --user-image-dir /avatars/ --highlight-all-users -s 0.5 -o /results/gource.ppm
+screen -dmS recording xvfb-run -a -s "-screen 0 ${RES}x${DEPTH}" gource "-$RES" -r 30 --title "$TITLE" --user-image-dir /avatars/ --highlight-all-users -s 0.5 --seconds-per-day ${SEC_PER_DAY:-1} --hide filenames -o /results/gource.ppm
 
 # This hack is needed because gource process doesn't stop
-sleep 5
-filesize=$(stat -c '%s' /results/gource.ppm)
-while [[ "$filesize" -eq "0" || $filesize -lt $(stat -c '%s' /results/gource.ppm) ]] ;
+lastsize="0"
+filesize="0"
+while [[ "$filesize" -eq "0" || $lastsize -lt $filesize ]] ;
 do
-	sleep 20
-	filesize=$(stat -c '%s' /results/gource.ppm)
+    sleep 20
+    lastsize="$filesize"
+    filesize=$(stat -c '%s' /results/gource.ppm)
     echo 'Polling the size. Current size is' $filesize
 done
 echo 'Force stopping recording because file size is not growing'
 screen -S recording -X quit
 
-xvfb-run -a -s "-screen 0 1280x720x24" ffmpeg -y -r 30 -f image2pipe -loglevel info -vcodec ppm -i /results/gource.ppm -vcodec libx264 -preset medium -pix_fmt yuv420p -crf 1 -threads 0 -bf 0 /results/gource.mp4
+xvfb-run -a -s "-screen 0 ${RES}x${DEPTH}" ffmpeg -y -r 30 -f image2pipe -loglevel info -vcodec ppm -i /results/gource.ppm -vcodec libx264 -preset medium -pix_fmt yuv420p -crf 1 -threads 0 -bf 0 /results/gource.mp4
 rm -f /results/gource.ppm
+
